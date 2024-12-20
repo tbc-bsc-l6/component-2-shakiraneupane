680cd913082e74dc31ee0255b250c909fc48a14f
shakiraneupane <sharmashakira47@gmail.com>
2024-12-20 13:39:36 +0545

    Initial commit- Database setup

---
+++ LaravelProject/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+require __DIR__.'/auth.php';
